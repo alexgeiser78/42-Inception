c06a94ed0448f62272e90c31920758149091af8c
Alexandre Geiser <ageiser@car8s2.42barcelona.com>
2024-10-19 16:32:46 +0200

    19.10.24

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -1,48 +1,42 @@
-#!/bin/bash
+#!/bin/bash  
+# we have to unse bash to run the script
 
- #First we check if the wp-config.php file exists
- #If it does not exist, we create it
- #If it does exist, we do nothing
- #wp config create: this command creates the wp-config.php file
- #wp core install: this command installs WordPress
- #wp user create: this command creates a new user
-
-if [ -f ./wp-config.php ]
+if [ -f ./wp-config.php ] # check if the default config file exists in folder /var/www/html
 then
 	echo -e "\033[0;31mWordPress is already installed\033[0m"
 else
-	wp core download --allow-root
-	wp config create --allow-root \
-		--dbname=$MYSQL_DATABASE \
+	wp core download --allow-root    #use the wp-cli to download the latest version of wordpress with root privileges
+	wp config create --allow-root \  #create the wp-config.php file with the following parameters 
+		--dbname=$MYSQL_DATABASE \   #-- is the flag to pass the parameter
 		--dbuser=$MYSQL_USER \
 		--dbpass=$MYSQL_PASSWORD \
-		--dbhost=mariadb
-	wp core install --url=$DOMAIN_NAME \
+		--dbhost=mariadb    	     #the host is the name of the service in the docker-compose file
+	wp core install --url=$DOMAIN_NAME \  #define the domain name of the website
 		--title="$SITE_TITLE" \
 		--admin_user=$ADMIN_USER \
 		--admin_password=$ADMIN_PASSWORD \
 		--admin_email=$ADMIN_EMAIL \
-		--skip-email \
+		--skip-email \                   #skip the email verification of installation
 		--allow-root
-	wp user create --allow-root \
+	wp user create --allow-root \      #create a new user with the following parameters
 		$USER_NAME \
 		$USER_EMAIL \
 		--user_pass=$USER_PASSWORD \
-		--role=author
-	wp theme install --allow-root \
+		--role=author                  #also: administrator(total control of the website), editor(supervisor of the content created by him or author), author(only write and publish his own posts), contributor(only write posts but can't publish), subscriber(only read posts)
+	wp theme install --allow-root \     #install the theme Rainfall
 		--activate \
-		neve
+		Rainfall
 	echo -e "\033[0;32mwp-config.php file created\033[0m"
 fi
 
-if [ ! -d /run/php ]
+if [ ! -d /run/php ] #check if the directory /run/php doesn't exists
 then
 	echo -e "\033[0;32mCreating /run/php directory\033[0m"
-	mkdir -p /run/php
-	chmod 755 /run/php
+	mkdir -p /run/php  # create the directory /run/php
+	chmod 755 /run/php  #change the permissions of the directory
 else
 	echo -e "\033[0;31m/run/php directory already exists\033[0m"
 fi
 
-# We start the php-fpm service in the foreground
-/usr/sbin/php-fpm7.3 -F
+
+/usr/sbin/php-fpm8.3 -F  #start the php-fpm service with the version 8.3 of php, -F means that PHP-FPM will not daemonize, and will stay in the foreground until the process is killed.
